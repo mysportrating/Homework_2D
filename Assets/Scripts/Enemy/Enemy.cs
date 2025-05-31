353113f76fe30073ff119664bd96b103dd2e4cf9
mysportrating <lipatov_ivan@list.ru>
2025-05-31 15:54:43 +0400

    Добавил логику преследования врагом игрока

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -23,48 +23,78 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Collider2D hit = Physics2D.OverlapBox(GetVisionArea(), _visionDistance, 0, _targetLayer);
-
-
-        if (hit != null)
+        // ќбнаружение игрока в зоне видимости и перемещение противника в направлении цели (заданна€ точка или игрок)
+        if (TrySeeTarget(out Transform target))
         {
-            Vector2 raycastDirection = (hit.transform.position - transform.position).normalized;
-            RaycastHit2D hit2D = Physics2D.Raycast(transform.position, raycastDirection, _visionDistance.x, ~(1 << gameObject.layer));
-
-            if (hit2D.collider != null)
-            {
-                if (hit2D.collider != hit)
-                    Debug.DrawLine(transform.position, hit2D.point, Color.white);
-                else
-                    Debug.DrawLine(transform.position, hit2D.point, Color.red);
-            }
+            Move(target);
         }
-        
-        Move();
+        else
+            Move(_target);
 
+        //»зменение целевой точки
         if (IsTargetReached())
             ChangeTarget();
     }
-    private void Move()
+
+    private bool TrySeeTarget(out Transform target)
     {
-        Vector2 newPosition = Vector2.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+        // ќбнул€ем значение таргета (цели)
+        target = null;
+
+        // ќбпределение области видени€ противника
+        Collider2D hit = Physics2D.OverlapBox(GetVisionArea(), _visionDistance, 0, _targetLayer);
+
+        if (hit != null)
+        {
+            // Ќаправл€ем луч в сторону цели (игрока) через Reycast
+            Vector2 raycastDirection = (hit.transform.position - transform.position).normalized;
+            RaycastHit2D hit2D = Physics2D.Raycast(transform.position, raycastDirection, _visionDistance.x, ~(1 << gameObject.layer));
+
+            // ѕровер€ем получилось ли у нас попасть в игрока
+            if (hit2D.collider != null)
+            {
+                // ¬изуальное отображение луча в сторону цели (игрока)
+                if (hit2D.collider == hit)
+                {
+                    Debug.DrawLine(transform.position, hit2D.point, Color.red);
+                    // ѕрисваеваем позицию игрока в качестве цели
+                    target = hit2D.transform;
+                    return true;
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, hit2D.point, Color.white);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void Move(Transform target)
+    {
+        // ѕеремещение противника через смещение позиции противника в сторону новой точки (цели)
+        Vector2 newPosition = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
         _rigidbody.MovePosition(newPosition);
     }
 
     private bool IsTargetReached()
     {
+        // ѕроверка достигнута ли цель (точка)
         float sqrDistance = (transform.position - _target.position).sqrMagnitude;
-
         return sqrDistance <= _maxSqrDistance;
     }
 
     private void ChangeTarget()
     {
+        // »нкремент дл€ смещение к новой точке в массиве точек пути противника
         _wayPointIndex++;
 
+        // ѕроверка выхода за пределы массива и обнуление индекса, если индекс вышел за пределы массива
         if (_wayPointIndex >= _wayPoints.Length)
             _wayPointIndex = 0;
 
+        // ѕрисваивание кординанат новой точки
         _target = _wayPoints[_wayPointIndex].transform;
 
         // —мена направлени€ взгл€да врага
@@ -77,7 +107,8 @@ public class Enemy : MonoBehaviour
 
     private Vector2 GetVisionArea()
     {
-        float halfCoefficient = 2;
+        //ќфсет (смещение) области видимости противника дл€ реалистичности
+        float halfCoefficient = 1.8f;
         int directionCoefficient = _isTurnedRight ? 1 : -1;
         float originX = transform.position.x + _visionDistance.x / halfCoefficient * directionCoefficient;
         return new Vector2(originX, transform.position.y);
@@ -85,6 +116,7 @@ public class Enemy : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // ¬изуальное отображеие области видимости противника
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(GetVisionArea(), _visionDistance);
     }
